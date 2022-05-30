e7eccc518f39a21e2fe5246bbaebd7b8b75b5b38
ruraeru <f93335383@gmail.com>
2022-05-30 22:27:43 +0900

    test class

---
+++ src/Test/Test.java
@@ -0,0 +1,20 @@
+package Test;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+class a {
+    int year, month, day;
+
+    public a(int... time) {
+        for (int i = 0; i < time.length; i++) {
+            System.out.println(time[i]);
+        }
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
